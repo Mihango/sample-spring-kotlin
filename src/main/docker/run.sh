928083e5732fe034267584d8aa73e1d433ea0112
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2019-07-20 11:36:35 +0300

    Docker config

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "**************************************"
+echo "Starting sample microservice"
+echo "**************************************"
+java -jar /usr/local/sample-microservice/@project.build.finalName@.jar
--- src/main/kotlin/com/sample/microservice/SampleMicroserviceApplication.kt
+++ src/main/kotlin/com/sample/microservice/SampleMicroserviceApplication.kt
@@ -3,12 +3,26 @@ package com.sample.microservice
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
-class SampleMicroserviceApplication
+@RestController
+@RequestMapping(value =["hello"])
+class SampleMicroserviceApplication {
+
+    @GetMapping(value = ["/{firstName}/{lastName}"], produces = ["application/json"])
+    fun hello(@PathVariable firstName: String, @PathVariable lastName: String): String {
+        return "{\"message\":\"Hello $firstName $lastName\"}"
+    }
+
+    @GetMapping(value = ["/me"])
+    fun sayMe(): String {
+        return "Say Hi"
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SampleMicroserviceApplication>(*args) {
-        this.setBannerMode(Banner.Mode.OFF)
+        setBannerMode(Banner.Mode.OFF)
     }
 }
